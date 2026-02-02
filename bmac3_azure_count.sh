811a317fcd2744bc94830f67a42c4ac3bdac6ed3
bmacdonald3 <benmacdonald3@gmail.com>
2026-02-02 15:10:43 -0500

    Update bmac3_azure_count.sh

--- bmac3_azure_count.sh
+++ bmac3_azure_count.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
-python3 -c "
-import sys, os; sys.path.insert(0, os.path.expanduser('~'))
-from config import *
+
+# metar_azure_count.sh - Get total METAR observations in Azure
+
+# Called by Home Assistant via SSH
+
+# Load environment variables
+
+set -a
+source “$HOME/.env”
+set +a
+
+# Query Azure SQL for count
+
+python3 << ‘EOF’
+import os
 import pyodbc
+
+AZURE_CONN_STR = (
+f”DRIVER={{ODBC Driver 18 for SQL Server}};”
+f”SERVER=flight-data-server-macdonaldfamily.database.windows.net;”
+f”DATABASE=Flightdata;”
+f”UID=flightadmin;”
+f”PWD={os.environ[‘AZURE_PASSWORD’]};”
+f”Encrypt=yes;”
+f”TrustServerCertificate=no;”
+f”Connection Timeout=30;”
+)
+
 try:
-    conn = pyodbc.connect(AZURE_CONN_STR)
-    cursor = conn.cursor()
-    cursor.execute('SELECT COUNT(*) FROM flights')
-    print(cursor.fetchone()[0])
-    conn.close()
-except:
-    print(-1)
-"
+conn = pyodbc.connect(AZURE_CONN_STR)
+cursor = conn.cursor()
+cursor.execute(“SELECT COUNT(*) FROM metar_observations”)
+count = cursor.fetchone()[0]
+print(count)
+cursor.close()
+conn.close()
+except Exception as e:
+print(f”Error: {e}”)
+EOF
